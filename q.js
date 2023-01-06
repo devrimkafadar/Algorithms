49d374bcfc6b91e528c4e03af52deb91cdfdeec1
Devrim Kafadar <108999645+devrimkafadar@users.noreply.github.com>
2023-01-06 19:33:32 +0300

    Update q.js

--- q.js
+++ q.js
@@ -10,28 +10,25 @@ const array4 = [100, 50, 200, 400, 20, 60, 10, 90, 300, 200];
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 function buyAndSellAlgorithm (array) {
     //console.log('func');
-    var amount = 0; //alınan yatırım gerecinin miktarını tutacak olan değişken
-    var profit = 0; //o anki karı tutacak olan değişken
-    var takenValue = 0; //satın alma işlemi yapılırkenki alış fiyatı
-    var has = false; //o an kullanıcının elinde herhangi bir yatırım gerecinin olup olmadığı
+    var amount = 0;
+    var profit = 0;
+    var takenValue = 0;
+    var has = false;
 
 
     for (let i=0;i<array.length;i++) {
-        if (i == array.length - 1) { //dizinin son elemanına ulaşılma durumu
-            if (has) { //eğer son gün elimizde varlık varsa satmamız gerekir.
+        if (i == array.length - 1) {
+            if (has) {
                 profit += (array.at(i) - takenValue) * amount;
                 has = false;
                 break;
             }
-            else break; //eğer son gün elimizde varlık yoksa herhangi bir işleme gerek yok
+            else break;
         }
 
-        if (array.at(i) > array.at(i+1) && !has) continue; //eğer ertesi gün daha düşük bir değer varsa ve elimizde varlık yoksa
-        //ertesi gün alım yapmak daha karlı olacağı için bugün alım yapmaya gerek yoktur.
+        if (array.at(i) > array.at(i+1) && !has) continue;
 
         if (array.at(i) < array.at(i+1) && !has) {
-            //eğer ertesi gün fiyatlar yükselecekse ve elimizde varlık yoksa, bugün alıp ertesi gün satmak karı artıracaktır.
-            //dolayısıyla alım yapmak gerekir.
             has = true;
             if (profit == 0) amount = 1;
             else amount = profit / array.at(i);
@@ -41,16 +38,12 @@ function buyAndSellAlgorithm (array) {
         }
 
         if (array.at(i) > array.at(i+1) && has) {
-            //eğer elimizde varlık varsa ve ertesi gün fiyatlar düşecekse halihazırda fiyatlar daha yüksekken satmak 
-            //maksimum kazanç sağlayacaktır.
             has = false;
             profit += (array.at(i) - takenValue) * amount;
             continue;
         }
 
         if (array.at(i) < array.at(i+1) && has) {
-            //eğer elimizde varlık varsa ve ertesi gün fiyatlar yükselecekse beklemek ve fiyatları o daha yüksek olan
-            // fiyattan satmak karı daha fazla artıracaktır.
             continue;
         }
     }
